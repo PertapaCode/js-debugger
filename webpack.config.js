2c829dcb694da21bb22616d8266c3c45f6f0a34b
Bruno Santos <brunoleugimm@gmail.com>
2016-10-23 18:55:22 +0200

    add files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/src',
+
+  entry: 'debug.js',
+
+  output: {
+    path: 'dist/',
+    filename: 'debug.js',
+    libraryTarget: 'umd',
+    library: 'debug',
+    umdNamedDefine: true
+  },
+
+  plugins: [
+    //new webpack.optimize.UglifyJsPlugin(),
+    //new webpack.HotModuleReplacementPlugin(),
+    //new webpack.optimize.CommonsChunkPlugin('index', 'index.js', null, 2),
+    //new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: __dirname + '/node_modules',
+      include: __dirname + '/src',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+
+  //debug: true,
+  //devtool: 'eval',
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'src'],
+    alias: {
+      vendor: __dirname + '/vendor'
+    }
+  }
+
+};
